3fa262532362881850d34a35d4d4c73ea721e4b3
Optixal <optixal@live.com.sg>
2017-05-21 17:31:46 +0800

    Added reliable, speed, speed ss3, speedevent ss3 3stageboss

---
+++ data.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+white = 16777215
+btnblue = 12550204
+
+clcongrats1 = (400, 940, 16777215) # left of white bar below congrats
+clcongrats2 = (1120, 940, 16777215) # right of white bar below congrats
+clcongrats3 = (360, 270, False) # health bar
+clresults = (720, 2375, 12550204) # center of blue button
+clexp = (720, 2375, 12550204) # center of blue button
+clchest = (721, 1411, 715552) # center of middle chest's green plumbob
+clgetitem = (720, 1757, 12550204)
+clretry = (1075, 2446, 10581296)
+clconfirm = (1020, 1830, 12550204)
+clloading = (350, 1800, 0)
+
+btattack = (1170, 1940)
+btresults = (720, 2375)
+btexp = (720, 2375)
+btchest = (720, 1660) # mid chest
+btgetitem = (720, 1757)
+btretry = (1075, 2446, 10581296)
+btconfirm = (1020, 1830)
+
